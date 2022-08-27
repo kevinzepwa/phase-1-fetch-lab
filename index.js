f0a67e3e7f34c3c8dd2873d3b6aeb1221e8bbb2f
kevinzepwa <zepwakevin8@gmail.com>
2022-08-27 10:16:17 +0300

    complete

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
 
-//Alternatively
+//*Alternatively
 // function fetchBooks() {
 //   // To pass the tests, don't forget to return your fetch!
 //   fetch('https://anapioficeandfire.com/api/books')
@@ -32,3 +32,13 @@ document.addEventListener('DOMContentLoaded', function() {
 //   h2.innerHTML = book.name;
 //   main.appendChild(h2);
 // }
+
+//*Refactoring the fetch fn
+// function fetchData(url, cb) {
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(data => cb(data))
+// }
+
+// const url =  'https://anapioficeandfire.com/api/books'
+// fetchData(url, renderBooks)
